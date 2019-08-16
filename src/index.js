97d05dd282a5a2c1a8971c4409bafcf784fef63a
Justin Hutsell <hutselljustin@gmail.com>
2019-08-16 12:23:25 -0400

    adds npm bootstrap and imports

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
