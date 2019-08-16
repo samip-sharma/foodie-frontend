9e740572ebb2b9caa32843feec61e42fbac62f13
Justin Hutsell <hutselljustin@gmail.com>
2019-08-16 12:25:18 -0400

    adds bootstrap and imports to npm

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
