635d861889dca58db1bf3294b51bea637043775e
github username <github email address>
2019-08-16 16:28:14 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,35 @@ import React from 'react';
 import './App.css';
 import Login from './components/Login'
 import ReturnMap from './components/Map'
+import Yelp from 'yelp'
 
 
 class App extends React.Component {
   state={
     name:'',
     user_name:'',
+    defaultQuery:'?term=food&location=nyc'
   }
 
   onLogin=(user_data)=>{
     console.log(user_data)
   }
 
-  componentDidMount
+  componentDidMount(){
+    var yelp = new Yelp({
+      consumer_key: 'cQIy3LDS7Xw7Xt6W-4iSW7rkaPuuv56XJ9UgQK1I0Y55Ks4BlJeys3j6B7Ewje2scBJEjbfCEId7dd2foDVpCLvGasb0vklV4ZEZBkJqmj_KTzIhNg3yh4P_pXBVXXYx',
+      consumer_secret: 'cQIy3LDS7Xw7Xt6W-4iSW7rkaPuuv56XJ9UgQK1I0Y55Ks4BlJeys3j6B7Ewje2scBJEjbfCEId7dd2foDVpCLvGasb0vklV4ZEZBkJqmj_KTzIhNg3yh4P_pXBVXXYx'
+    });
+
+
+  yelp.search({ term: 'food', location: 'Montreal' })
+  .then(function (data) {
+  console.log(data);
+})
+.catch(function (err) {
+  console.error(err);
+});
+  }
 
   render(){
     // console.log(this.state)
