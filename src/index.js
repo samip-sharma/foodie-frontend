30379f11c5661d73fedc33ffd6811370d5ba47a0
github username <github email address>
2019-08-17 09:52:22 -0400

    merge

--- src/index.js
+++ src/index.js
@@ -3,15 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-<<<<<<< HEAD
-import { BrowserRouter} from 'react-router-dom'
-
-ReactDOM.render(
-    <BrowserRouter>
-<App />
-    </BrowserRouter >
-    , document.getElementById('root'));
-=======
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
@@ -22,7 +13,6 @@ ReactDOM.render(
     <BrowserRouter>
         <App />
     </BrowserRouter>, document.getElementById('root'));
->>>>>>> 2cfecc1ae96beac9c840ad165c8fbb1f2dc911b4
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
