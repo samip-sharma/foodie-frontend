fb2a1d08c0b912585a70379f2d9615ad7f9c87db
github username <github email address>
2019-09-18 19:14:36 -0400

    Updates

---
+++ precache-manifest.eb2eb274442d3e547d095bbc85e7b094.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9048d1ba71c487ffebbe22bf895dd34",
+    "url": "/foodie-frontend/index.html"
+  },
+  {
+    "revision": "368c454c07dc5643aa43",
+    "url": "/foodie-frontend/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "0e392f32586f6dbff7b3",
+    "url": "/foodie-frontend/static/css/main.7e789bd3.chunk.css"
+  },
+  {
+    "revision": "368c454c07dc5643aa43",
+    "url": "/foodie-frontend/static/js/2.0331ae2f.chunk.js"
+  },
+  {
+    "revision": "0e392f32586f6dbff7b3",
+    "url": "/foodie-frontend/static/js/main.50a248f5.chunk.js"
+  },
+  {
+    "revision": "85f6e45369cf2c1e09d8",
+    "url": "/foodie-frontend/static/js/runtime~main.eba4a3b3.js"
+  },
+  {
+    "revision": "0d61350b794662bb29183a39cf0e1044",
+    "url": "/foodie-frontend/static/media/abc.0d61350b.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/foodie-frontend/precache-manifest.eb2eb274442d3e547d095bbc85e7b094.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/foodie-frontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
